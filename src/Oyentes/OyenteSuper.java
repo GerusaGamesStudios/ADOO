6027e7a9cecadfb12ff937279a55497244efcb0c
GerusaGamesStudios <gerar_99@hotmail.es>
2019-11-12 11:06:55 -0600

    TODOOOO

---
+++ src/Oyentes/OyenteSuper.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Oyentes;
+
+import java.awt.event.*;
+
+/**
+ *
+ * @author gerusagames
+ */
+public class OyenteSuper{
+    void actionPerformed(ActionEvent e){
+        
+    }
+}
